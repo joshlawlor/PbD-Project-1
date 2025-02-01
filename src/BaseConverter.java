091570d5c35e9abf974db1f2b8232d34f15838af
joshlawlor <joshlawlor1@gmail.com>
2025-02-01 14:19:13 -0500

    Extracted value and radix using substring. Printed each argument

--- src/BaseConverter.java
+++ src/BaseConverter.java
@@ -5,8 +5,11 @@ public class BaseConverter {
     public static void processArguments(String arg) {
         //First I need to find the index of the colon ":"
         int colonIndex = arg.indexOf(":");
-        System.out.println("Colon Index = " + colonIndex);
+        //System.out.println("Colon Index = " + colonIndex);
         //Second I need to extract the value on the left and the radix on the right (using substring)
+        String value = arg.substring(0, colonIndex);
+        String radixString = arg.substring(colonIndex + 1);
+        System.out.println("Value: " + value + " Radix: " + radixString);
         //Third I need to convert the extracted value to base 10
         //Lastly I need to print it out nice and neat like project example
     }
