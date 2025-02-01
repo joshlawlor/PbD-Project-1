6a573dcffb208ab7a834326448ea5034dba2f5b7
joshlawlor <joshlawlor1@gmail.com>
2025-02-01 14:12:03 -0500

    Pseudocode for main processArgs method

--- src/BaseConverter.java
+++ src/BaseConverter.java
@@ -2,6 +2,13 @@ import java.util.Arrays;
 
 public class BaseConverter {
 
+    public static void processArguments(String arg) {
+        //First I need to find the index of the colon ":"
+        //Second I need to extract the value on the left and the radix on the right (using substring)
+        //Third I need to convert the extracted value to base 10
+        //Lastly I need to print it out nice and neat like project example
+    }
+
     public static void main(String[] args) {
         System.out.println("args = " + Arrays.toString(args));
     }
