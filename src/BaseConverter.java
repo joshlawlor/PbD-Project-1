67283d01336c1e491de41d7303dc958ae84b5686
joshlawlor <joshlawlor1@gmail.com>
2025-02-01 14:17:28 -0500

    Find colonIndex, call processArgs method in main. Print out each index

--- src/BaseConverter.java
+++ src/BaseConverter.java
@@ -4,6 +4,8 @@ public class BaseConverter {
 
     public static void processArguments(String arg) {
         //First I need to find the index of the colon ":"
+        int colonIndex = arg.indexOf(":");
+        System.out.println("Colon Index = " + colonIndex);
         //Second I need to extract the value on the left and the radix on the right (using substring)
         //Third I need to convert the extracted value to base 10
         //Lastly I need to print it out nice and neat like project example
@@ -11,5 +13,10 @@ public class BaseConverter {
 
     public static void main(String[] args) {
         System.out.println("args = " + Arrays.toString(args));
+        int i = 0;
+        while (i < args.length) {
+            processArguments(args[i]);
+            i++;
+        }
     }
 }
